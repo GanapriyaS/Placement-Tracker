bb907857c3d1105243ae45b96faf2632c74bed14
aarthi0205 <aarthikesavan02@gmail.com>
2022-04-29 00:49:24 +0530

    added home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,91 +1,22 @@
 import React from 'react'
-
+import StudentDetails from '../student/studentDetails'
 const Home = () => {
   return (
     <>
   
-    <div className="w-full p-4 h-auto lg:h-screen pt-8 overflow-y-auto ">
-    <div className="grid justify-items-center ">
-    
-    <div className="rounded-full bg-gray-400 px-5 ">
-        <p className="text-5xl">A</p>
-    </div>
-    <p className="font-bold">ALex Del</p>
-    <p className="bg-black rounded-full text-white py-1 px-2">District Admin</p>
+    <div className="w-full p-4 h-auto lg:h-screen pt-8 overflow-y-auto mt-6 px-10 ">
+    <div className="flex justify-center pb-5">
+  <div className="">
+    <a href="/#" className="flex items-center text-2xl lg:text-5xl font-bold ">
+      <span className="text-black ">GOVERNMENT COLLEGE OF TECHNOLOGY</span>
+    </a>
+  </div>
+</div><br></br>
+<p className="font-bold text-justify text-sm lg:text-lg ">     The institute offers undergraduate courses leading to the degree of Bachelor of Engineering and postgraduate courses award the Master of Engineering degree. The Institution affiliated to Anna University, Chennai offers 9 Full-Time Under Graduate programmes, 4 Part-Time Under Graduate programmes and 11 Post Graduate programmes. The Institution has been exercising academic autonomy since 1987.All the Departments are recognized as center’s for research by Anna University.7 Under Graduate Programmes and 3 Post Graduate Programmes are accredited by the National Board of Accreditation.Undergraduate students are admitted based on competitive student rankings in higher secondary examination.
+ </p>
 
-</div>
 <br></br><br></br>
-
-<form className=" lg:py-0 py-5  grid justify-center" action="#">
-  <p className="font-bold">Profile</p>
-  <div className="flex items-center mb-3 ">
-      <div>
-        <label htmlFor="code">First Name</label>
-        <div className="flex items-center mb-3 bg-white rounded mr-2">
-             
-          <input className="w-full  pl-4 pr-6 py-4 font-bold rounded  focus:outline-none bg-white" id="code" type="text" placeholder="FirstName" />
-        </div>
-      </div>
-     <div>
-      <label htmlFor="number">LastName</label>
-      <div className="flex items-center mb-3 bg-white rounded ml-2">
-       
-     <input className="w-full  pl-4 pr-6 py-4 font-bold rounded  focus:outline-none bg-white" id="number" type="text" placeholder="LastName" />
-   </div>
-     </div>
-  </div>
-
-  <div className="flex items-center mb-3 ">
-      <div>
-        <label htmlFor="code">Email</label>
-        <div className="flex items-center mb-3 bg-white rounded mr-2">
-             
-          <input className="w-full  pl-4 pr-6 py-4 font-bold rounded  focus:outline-none bg-white" id="code" type="email" placeholder="Email" />
-        </div>
-      </div>
-     <div>
-      <label htmlFor="number">Birthdate</label>
-      <div className="flex items-center mb-3 bg-white rounded ml-2">
-       
-     <input className="w-full  pl-4 pr-6 py-4 font-bold rounded  focus:outline-none bg-white" id="number" type="date" placeholder="Birthdate" />
-   </div>
-     </div>
-  </div>
-<br></br>
-<br></br>
-<p className="font-bold">Password</p>
-<div>
-  <label htmlFor="code">Previous Password</label>
-  <div className="flex items-center mb-3 bg-white rounded mr-2">
-       
-    <input className="w-full  pl-4 pr-6 py-4 font-bold rounded  focus:outline-none bg-white" id="code" type="text" placeholder="PhoneNumber" />
-  </div>
-</div>
-  <div className="flex items-center mb-3 ">
-      <div>
-        <label htmlFor="code">Old Password</label>
-        <div className="flex items-center mb-3 bg-white rounded mr-2">
-             
-          <input className="w-full  pl-4 pr-6 py-4 font-bold rounded  focus:outline-none bg-white" id="code" type="text" placeholder="PhoneNumber" />
-        </div>
-      </div>
-
-      <div>
-          <label htmlFor="code">New Password</label>
-          <div className="flex items-center mb-3 bg-white rounded mr-2">
-               
-            <input className="w-full  pl-4 pr-6 py-4 font-bold rounded  focus:outline-none bg-white" id="code" type="text" placeholder="PhoneNumber" />
-          </div>
-        </div>
-      
-  </div>
- 
- 
-  
-   
-    <br></br>
-    <button className="py-4 px-6 bg-black hover:bg-black-600 text-white font-bold rounded transition duration-200">Update</button>
-  </form>
+<StudentDetails />
 
     </div>
     </>
